d172cf48e7b008cccac685fee2ed40038d6a91e0
6024918 <luyuanshu818@gmail.com>
2025-05-04 20:45:56 +0800

    2st commit
    modified hellp_world.py, added "greet(name)" function
    added requirements.txt

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
 print("Hello Word!")
+
+# define a function for greeting Keitaro
+def greet(name):
+  print(f"Hello! {name.title()}")
+greet(Keitaro)
---
+++ requirements.txt
@@ -0,0 +1,6 @@
+numpy
+pillow
+panda
+torch
+torchvision
+torchaudio
