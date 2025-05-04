94b8f5884d5cf18fc6a1eb541d5f910dd2b3af3e
6024918 <luyuanshu818@gmail.com>
2025-05-04 20:59:18 +0800

    3st commit
    modified hello_world.py: bug fixed, added ":" for Keitaro
                     added Hiro greeting loop

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,16 @@ print("Hello Word!")
 # define a function for greeting Keitaro
 def greet(name):
   print(f"Hello! {name.title()}")
-greet(Keitaro)
+greet("Keitaro")
+
+# make Hiro greeting loop
+number = 0
+ACTIVATE = True
+while ACTIVATE:
+  if number <= 5:
+    greet("Hiro")
+    number += 1
+  else:
+    ACTIVATE = False
+print("Finished greeting Hiro!")
+    
