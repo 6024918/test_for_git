bf78afa902b86f22507dd0ccf3d3cd431a386050
6024918 <luyuanshu818@gmail.com>
2025-05-04 21:36:37 +0800

    dev 1st commit
    modified hello_world.py: bug fixed, added ":" for Keitaro
                                                 added kissing function

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,14 @@ print("Hello Word!")
 # define a function for greeting Keitaro
 def greet(name):
   print(f"Hello! {name.title()}")
-greet(Keitaro)
+greet("Keitaro")
+
+# define a fuction for kissing
+def kiss(name, time):
+  if time >1:
+    s = "times"
+  else:
+    s = "time"
+  print(f"{name.title()}, I want to kiss you {time} {s}!")
+kiss("hunter", 1)
+kiss("taiga", 4)
